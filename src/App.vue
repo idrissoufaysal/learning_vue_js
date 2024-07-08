f97e34e9c05ccd2ebcbe3a2d52ed0e8dd52a10e5
idrissou <idrissoufaysal93@.com>
2024-07-08 09:52:23 +0000

    add

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-    welcome
+
+  <div class="text-blue-500 text-2xl bg-slate-500 flex ">
+
+    welcome hello dsa
+    <Home />
   </div>
+
+
 </template>
 <script>
+import Home from './views/Home.vue';
+export default {
+  components: {
+    Home
+  }
+}
 
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        this is the home page
+    </div>
+</template>
